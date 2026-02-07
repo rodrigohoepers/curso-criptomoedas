745348bc84f63f9706525872a08b47e24b68452a
rodrigohoepers <rodrigohoe@gmail.com>
2026-02-07 15:12:28 -0300

    Add/Update tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        slate: {
+          950: '#0f172a',
+        },
+      },
+    },
+  },
+  plugins: [
+    require('@tailwindcss/typography'),
+  ],
+  darkMode: 'class',
+}
